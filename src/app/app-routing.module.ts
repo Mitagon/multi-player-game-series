c8699b1749d18153fc4446a55e367f1f2616c266
Ike-Njoku David <ikenjokudc@gmail.com>
2022-06-01 02:10:49 +0100

    Generated Dashboard Module (Angular CLI)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./index/index.module').then(m => m.IndexModule) }, { path: 'create-room', loadChildren: () => import('./create-room/create-room.module').then(m => m.CreateRoomModule) }];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./index/index.module').then(m => m.IndexModule) },
+  { path: 'create-room', loadChildren: () => import('./create-room/create-room.module').then(m => m.CreateRoomModule) },
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
