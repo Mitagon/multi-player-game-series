64d3ea33ba4ff58c6ea7c2a770a230796e998e48
Ike-Njoku David <ikenjokudc@gmail.com>
2022-06-08 00:30:05 +0100

    UI updates on Create Game Component

--- src/app/dashboard/create-game/create-game.component.ts
+++ src/app/dashboard/create-game/create-game.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { GameService } from './game.service';
 
 @Component({
   selector: 'app-create-game',
@@ -7,7 +8,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./create-game.component.css']
 })
 export class CreateGameComponent implements OnInit {
-
+  submitting: boolean = false;
   createGameForm: FormGroup = this.formBuilder.group({
     name: [null, Validators.required],
     description: [null, Validators.required],
@@ -16,14 +17,26 @@ export class CreateGameComponent implements OnInit {
   });
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private gameService: GameService
   ) { }
 
   ngOnInit(): void {
   }
 
   createGame() {
-
+    this.submitting = true;
+    this.gameService.createGame(this.createGameForm.value)
+      .subscribe(
+        (response) => {
+          console.log(response);
+          this.submitting = false;
+        },
+        (error: string) => {
+          console.log(error)
+          this.submitting = false;
+        }
+      )
   }
 
 }
--- src/app/dashboard/create-game/game.service.ts
+++ src/app/dashboard/create-game/game.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { ServerResponseDto } from 'src/app/server-response-dto';
+import { CreateGameDto } from './create-game-dto';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +13,18 @@ export class GameService {
     private http: HttpClient
   ) { }
 
-  createGame() {
+  createGame(game: CreateGameDto): Observable<ServerResponseDto> {
+    const url = '';
+    return this.http.post<ServerResponseDto>(url, game)
+      .pipe(
+        (catchError(this.handleError))
+      )
+  }
 
+  private handleError(error: any) {
+    console.log(error);
+    return throwError((error: any) => new Error('Could not complete Action'+
+    'Please Check Console for more details.'
+    ));
   }
 }
